d77b706e948050dca07d92fef9633d36418341e0
Jean Pierre <hjepih@gmail.com>
2022-09-28 19:09:56 -0300

    update: change webpack folder export

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "cdn"),
     filename: 'idealvalues.js'
   }
 };
